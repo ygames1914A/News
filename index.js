948af57943497c0c215cd3d03422520066be6bf8
oandygames.ygames <oandygames.ygames@gmail.com>
2026-06-05 16:01:58 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const API_KEY = `88bf36fa19025cef6c0c80cb59985cf3`
+
+async function fetchNews(category = 'general') {
+    let url = `https://gnews.io/api/v4/top-headlines?category=${category}&lang=ar&country=eg&max=10&apikey=${API_KEY}`
+
+    try {
+        const response = await fetch(url)
+        let data = await response.json()
+        console.log(data.articles);
+        render(data.articles)
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+function render(articles) {
+    const grid = document.getElementById('news-grid')
+
+    articles.forEach(articles => {
+        let card = document.createElement("div")
+        card.className = 'news-card'
+        card.innerHTML = `
+                    <div class="img-container">
+                        <img src="${articles.image}" alt="Photo of the article">
+                    </div>
+
+                    <div class="news-content">
+                        <h3>${articles.title}</h3>
+                        <p>${articles.content}</p>
+                    </div>
+        `
+
+        grid.appendChild(card)
+
+    });
+
+}
+
+
+fetchNews()
